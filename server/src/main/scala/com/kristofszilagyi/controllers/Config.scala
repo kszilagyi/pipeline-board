9fe5f26768e04ace22a3da513f88259a745a4041
kszilagyi <kristof.szilagyi1@gmail.com>
2017-10-19 20:41:24 +0100

    Style

--- server/src/main/scala/com/kristofszilagyi/controllers/Config.scala
+++ server/src/main/scala/com/kristofszilagyi/controllers/Config.scala
@@ -5,7 +5,7 @@ import net.jcazevedo.moultingyaml._
 import net.jcazevedo.moultingyaml.DefaultYamlProtocol._
 
 object JobConfig {
-  def wrappedYamlString[T](fromString: String => T)(convertToString: T => String) = new YamlFormat[T] {
+  def wrappedYamlString[T](fromString: String => T)(convertToString: T => String): YamlFormat[T] = new YamlFormat[T] {
     def read(yaml: YamlValue): T = yaml match {
       case YamlString(s) => fromString(s)
       case other => deserializationError(s"Should be a string got: $other")
