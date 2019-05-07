78e84fa3e75dca3aced56fb40aa27150cf59076f
Peter Robinson <peter.robinson@ocado.com>
2019-05-07 10:58:14 +0100

    Proper granularity of secrets-not-in-toString

--- server/src/test/scala/com/kristofszilagyi/pipelineboard/controllers/ConfigTest.scala
+++ server/src/test/scala/com/kristofszilagyi/pipelineboard/controllers/ConfigTest.scala
@@ -8,9 +8,8 @@ object ConfigTest extends TestSuite {
     // As well as testing what it does on the surface, I also write this trivial test to check that the custom toString doesn't
     // throw. Sadly that is not a given since class.getSimpleName certainly does sometimes.
     'noSecretsInToString {
-      // TODO: this is unnecessarily brittle - I don't want to assert on the exact string, but merely that it doesn't include the secret.
-      JenkinsAccessToken("secret").toString ==> "com.kristofszilagyi.pipelineboard.controllers.JenkinsAccessToken"
-      GitLabCiAccessToken("secret").toString ==> "com.kristofszilagyi.pipelineboard.controllers.GitLabCiAccessToken"
+      assert(!(JenkinsAccessToken("jenkinssecret").toString contains "secret"))
+      assert(!(GitLabCiAccessToken("gitlabsecret").toString contains "secret"))
     }
   }
 }
